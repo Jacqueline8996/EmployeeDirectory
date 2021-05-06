610ab9cc6e675efc2890d3b908773db2186349ec
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-05-06 02:42:13 -0400

    added in the base employee

---
+++ src/component/page/EmployeeDirect.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PageWrapper from "../PageWrapper/PageWrapper"
+
+
+const EmployeeDirect= () => {
+    return (
+      <div>
+        <PageWrapper>
+        </PageWrapper>
+      </div>
+    );
+  };
+  
+  export default EmployeeDirect;
